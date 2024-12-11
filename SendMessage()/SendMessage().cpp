28f088ac0afca85cbc3beb88d0194d89f67bac16
Franco Caraffa <francocaraffa2005@gmail.com>
2024-12-11 15:42:48 -0300

    Pulsar teclas en ventana especifica

---
+++ SendMessage()/SendMessage().cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+
+#define F2 0x3C0000
+
+using namespace std;
+
+int main() {
+	HWND cs = FindWindow(0, "Counter-Strike");
+	cout << cs;
+	Sleep(5000);
+	
+	// PULSAR TECLA F2  
+	SendMessage(cs, WM_KEYDOWN, F2, F2);
+	Sleep(1);
+	SendMessage(cs, WM_KEYUP, F2, F2);
+	
+	return 0;
+}
+
